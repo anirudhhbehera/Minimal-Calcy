3d41aa03f8ad40cba253b693aa997dd9284c7496
Anirudhh behera <85536981+anirudhhbehera@users.noreply.github.com>
2023-02-07 03:11:31 +0530

    Adding files

---
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.demo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.but)
+        button.setOnClickListener {
+            val intent = Intent(applicationContext, MainActivity2::class.java)
+            startActivity(intent)
+        }
+    }
+}
